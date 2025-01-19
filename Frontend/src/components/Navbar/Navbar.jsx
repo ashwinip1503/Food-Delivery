bdb009a520fee2877a6a59a4fcf8a9255d066f38
ashwinip1503 <ashwinipadwal0315@gmail.com>
2025-01-19 19:13:03 +0530

    Setting up

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+
+        
+    </div>
+  )
+}
+
+export default Navbar
