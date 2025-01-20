eb5bc24f040bc91fb109e1ce480295e87f55715d
ashwinip1503 <ashwinipadwal0315@gmail.com>
2025-01-20 18:56:22 +0530

    ExploreMenu

---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Experience the ultimate convenience with our food delivery website, where you can explore a wide variety of cuisines, order your favorite meals with ease, and enjoy fast, reliable delivery straight to your doorstep—all in just a few clicks!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
