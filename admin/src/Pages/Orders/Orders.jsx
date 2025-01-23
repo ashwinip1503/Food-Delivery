5cb926d9a79979cdec3fd5bad37aad6c9f6792d3
ashwinip1503 <ashwinipadwal0315@gmail.com>
2025-01-23 17:48:59 +0530

    Corrected Order

--- admin/src/Pages/Orders/Orders.jsx
+++ admin/src/Pages/Orders/Orders.jsx
@@ -9,16 +9,22 @@ const Orders = ({ url }) => {
   const [orders, setOrders] = useState([]);
 
   const fetchAllOrders = async () => {
-    
+    try {
       const response = await axios.get(`${url}/api/order/list`);
       if (response.data.success) {
-        setOrders(response.data.data);
-        console.log(response.data.data);
+        if (Array.isArray(response.data.data)) {
+          setOrders(response.data.data);
+        } else {
+          setOrders([response.data.data]);
+        }
       } else {
         toast.error('Error');
       }
-    } 
-  
+    } catch (error) {
+      console.error(error);
+      toast.error('Error fetching orders');
+    }
+  }
 
   useEffect(() => {
     fetchAllOrders();
@@ -28,27 +34,38 @@ const Orders = ({ url }) => {
     <div className='order add'>
       <h3>Order Page</h3>
       <div className="order-list">
-        {orders.map((order,index)=>(
+        {Array.isArray(orders) && orders.map((order, index) => (
           <div key={index} className="order-item">
-           <img src={assets.parcel_icon} alt="" />
-           <div>
-            <p className='order-item-food'>
-              {order.items.map((item,index)=>{
-                if (index===order.items.length-1) {
-                  return item.name + ' x' + item.quantity
-                  
-                }
-                else {
-                  return item.name + 'x ' + item.quantity +","
-                }
-              })}
-            </p>
-           </div>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              {Array.isArray(order.items) && (
+                <p className='order-item-food'>
+                  {order.items.map((item, index) => {
+                    if (index === order.items.length - 1) {
+                      return item.name + ' x' + item.quantity
+                    }
+                    else {
+                      return item.name + 'x ' + item.quantity + ","
+                    }
+                  })}
+                </p>
+              )}
+              {order.address && (
+                <React.Fragment>
+                  <p className="order-item-name">{order.address.firstName + "" + order.address.lastName}</p>
+                  <div className="order-item-address">
+                    <p>{order.address.street + ","}</p>
+                    <p>{order.address.city + "," + order.address.state + "," + order.address.country + "," + order.address.zipCode}</p>
+                  </div>
+                  <p className='order.item.phone'>{order.address.phone}</p>
+                </React.Fragment>
+              )}
+            </div>
+            {order.items && <p>Items:{order.items.length}</p>}
+            {order.amount && <p>${order.amount}</p>
+            }
           </div>
-        )
-          
-        )}
-       
+        ))}
       </div>
     </div>
   );
