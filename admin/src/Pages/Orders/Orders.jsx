34219cb4206740d8103d5289991a5b8ac2177c08
ashwinip1503 <ashwinipadwal0315@gmail.com>
2025-01-23 15:13:57 +0530

    admin panel

--- admin/src/Pages/Orders/Orders.jsx
+++ admin/src/Pages/Orders/Orders.jsx
@@ -9,7 +9,7 @@ const Orders = ({ url }) => {
   const [orders, setOrders] = useState([]);
 
   const fetchAllOrders = async () => {
-    try {
+    
       const response = await axios.get(`${url}/api/order/list`);
       if (response.data.success) {
         setOrders(response.data.data);
@@ -17,10 +17,8 @@ const Orders = ({ url }) => {
       } else {
         toast.error('Error');
       }
-    } catch (error) {
-      toast.error('Error fetching orders');
-    }
-  };
+    } 
+  
 
   useEffect(() => {
     fetchAllOrders();
@@ -30,22 +28,27 @@ const Orders = ({ url }) => {
     <div className='order add'>
       <h3>Order Page</h3>
       <div className="order-list">
-        {Array.isArray(orders) && orders.map((order, index) => (
-          <div key={index} className='order-item'>
-            <img src={assets.parcel_icon} alt="" />
-            <div>
-              <p className='order-item-food'>
-                {order.item.map((item, idx) => {
-                  if (idx === order.item.length - 1) {
-                    return `${item.name} X ${item.quantity}`;  
-                  } else {
-                    return `${item.name} X ${item.quantity}, `;
-                  }
-                })}
-              </p>
-            </div>
+        {orders.map((order,index)=>(
+          <div key={index} className="order-item">
+           <img src={assets.parcel_icon} alt="" />
+           <div>
+            <p className='order-item-food'>
+              {order.items.map((item,index)=>{
+                if (index===order.items.length-1) {
+                  return item.name + ' x' + item.quantity
+                  
+                }
+                else {
+                  return item.name + 'x ' + item.quantity +","
+                }
+              })}
+            </p>
+           </div>
           </div>
-        ))}
+        )
+          
+        )}
+       
       </div>
     </div>
   );
